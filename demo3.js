4c3e63b61a649a220f453c2ed108ec22e1420417
Ankithachowdary <92664493+Ankithachowdary@users.noreply.github.com>
2022-01-13 21:32:21 +0530

    Add files via upload

---
+++ demo3.js
@@ -0,0 +1,20 @@
+let x = document.getElementById("sign_in");
+let y = document.getElementById("sign_up");
+let z = document.getElementById("calculator")
+
+function signIn() {
+  y.style.display = "none";
+  x.style.display = "revert";
+  
+}
+function signUp() {
+  x.style.display = "none";
+  y.style.display = "revert";
+  
+  
+}
+function calculator(){
+    
+    z.style.display = "revert";
+    
+}
